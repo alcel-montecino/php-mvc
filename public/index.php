0965b554790a6e5fdcf5ff9dcfa63b849838ea89
Alcel Montecino <118526787+alcel-montecino@users.noreply.github.com>
2026-01-27 23:50:29 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,11 +4,20 @@
 // Start session first thing
 session_start();
 
-require_once '../core/App.php';
-require_once '../core/Controller.php';
-require_once '../core/Database.php';
-require_once '../core/Model.php';
-require_once '../core/Auth.php';
+// Use for local
+// require_once '../core/App.php';
+// require_once '../core/Controller.php';
+// require_once '../core/Database.php';
+// require_once '../core/Model.php';
+// require_once '../core/Auth.php';
+
+// Use for replit deployment
+// Use absolute paths based on current file location
+require_once __DIR__ . '/../core/App.php';
+require_once __DIR__ . '/../core/Controller.php';
+require_once __DIR__ . '/../core/Database.php';
+require_once __DIR__ . '/../core/Model.php';
+require_once __DIR__ . '/../core/Auth.php';
 
 // Run app
 $app = new App();
